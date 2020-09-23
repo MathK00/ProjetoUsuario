73f9cda897a867c1060c9c8e5a5c11efab56cfd6
Kaique <matheuskaique00@gmail.com>
2020-09-22 22:20:09 -0300

    Backend com Insert

---
+++ src/routes/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const UsuarioController = require ('../controllers/usuarioController.js');
+const router = express.Router();
+ 
+router.post('/usuarios', UsuarioController.Insert);
+ 
+module.exports = router;
+
